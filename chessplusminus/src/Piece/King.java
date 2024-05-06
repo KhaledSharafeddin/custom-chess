c23b2c8c6398ea44fb3f99630ff2eb6b94a1f6a4
emirmut1903 <emir.mut@ozu.edu.tr>
2024-05-07 00:05:39 +0300

    King and Knight classes

--- chessplusminus/src/Piece/King.java
+++ chessplusminus/src/Piece/King.java
@@ -1,5 +1,17 @@
 package Piece;
 
 public class King {
+    private Game.Type type;
 
+    public King(Game.Box box, Game.Player player) {
+
+    }
+
+    public Game.Type getType() {
+        
+    }
+
+    public boolean isValidMove(Game.Box destinationBox) {
+        
+    }
 }
--- chessplusminus/src/Piece/Knight.java
+++ chessplusminus/src/Piece/Knight.java
@@ -1,5 +1,17 @@
 package Piece;
 
 public class Knight {
+    private Game.Type type;
 
+    public Knight(Game.Box box, Game.Player player) {
+
+    }
+
+    public Game.Type getType() {
+        
+    }
+
+    public boolean isValidMove(Game.Box destinationBox) {
+        
+    }
 }
