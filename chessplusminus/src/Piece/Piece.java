fce4febf0601e5357cef0f172174756db2d0b2a2
emirmut1903 <emir.mut@ozu.edu.tr>
2024-05-06 23:46:20 +0300

    interface

--- chessplusminus/src/Piece/Piece.java
+++ chessplusminus/src/Piece/Piece.java
@@ -1,5 +1,13 @@
 package Piece;
 
-public interface Piece {
+import Game.Player;
+import Game.Box;
 
+public interface Piece {
+    public static final Box box = new Box();
+    public static final Player player = new Player();
+    Player getPlayer();
+    Box getBox();
+    void setBox(Box destinationBox);
+    boolean isValidMove(Box destinationBox);
 }
