32d9803f1d94c16fd84f443d867a3146ce0c2fc1
khaledsharaff <khaled.sh.02@gmail.com>
2024-05-07 00:21:11 +0300

    Added frame and board for GUI

--- chessplusminus/src/GUI/ChessBoardGui.java
+++ chessplusminus/src/GUI/ChessBoardGui.java
@@ -1,5 +1,27 @@
 package GUI;
 
-public class ChessBoardGui {
+import javax.swing.*;
+import java.awt.*;
 
+public class ChessBoardGui extends JPanel {
+    
+    public int squareSize = 85;
+    int cols = 8;
+    int rows = 8;
+
+    public ChessBoardGui(){
+        this.setPreferredSize(new Dimension(cols * squareSize, rows * squareSize));
+        this.setBackground(Color.green);
+    }
+
+    public void paintComponent(Graphics g) {
+        Graphics2D g2d = (Graphics2D) g;
+
+        for(int r = 0; r < rows; r++)
+            for(int c = 0; c<cols; c++){
+                g2d.setColor((c + r) % 2 == 0 ? new Color(67,103,149) : new Color(146,205,255));
+                g2d.fillRect(c*squareSize, r*squareSize, squareSize, squareSize);
+            }
+        
+    }
 }
--- chessplusminus/src/GUI/GUI.java
+++ chessplusminus/src/GUI/GUI.java
@@ -1,5 +1,24 @@
 package GUI;
 
-public interface GUI {
+import javax.swing.*;
+import java.awt.*;
 
+public interface GUI {
+    public static void main(String[] args) {
+        // Create the main frame
+        JFrame frame = new JFrame("C+-: Custom Chess");
+        frame.setLayout(new GridBagLayout());
+
+        // Maximize the frame to fullscreen
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH);
+
+        // Create and add the chess board panel
+        ChessBoardGui board = new ChessBoardGui();
+        frame.add(board);
+
+        // Configure the frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setResizable(false); // Optional: You can set this to true if you want to allow resizing
+        frame.setVisible(true);
+    }
 }
