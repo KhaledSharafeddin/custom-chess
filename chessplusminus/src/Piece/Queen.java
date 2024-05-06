2f8f06ea8bf112e8503298dc3bb73a98f14dda25
Raneem <raneemabdulbar@gmail.com>
2024-05-07 00:07:34 +0300

    Queen

--- chessplusminus/src/Piece/Queen.java
+++ chessplusminus/src/Piece/Queen.java
@@ -1,5 +1,19 @@
 package Piece;
 
 public class Queen {
+    private static Game.Type type;
+
+    public Queen(Game.Box game, Game.Player player){
+
+    } 
+
+    public static Game.Type getType(){
+        return type;
+    }
+
+    public static boolean isValidMove(Game.Box destinationBox){
+        //TODO: do the case 
+        return false;
+    }
 
 }
