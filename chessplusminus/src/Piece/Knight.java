3c6c23750fb47a9dd7213afad2dc023fc96aa18c
Raneem <raneemabdulbar@gmail.com>
2024-05-07 22:58:41 +0300

    knight movement

--- chessplusminus/src/Piece/Knight.java
+++ chessplusminus/src/Piece/Knight.java
@@ -1,17 +1,45 @@
 package Piece;
 
+import Game.Box;
+import Game.Player;
+
 public class Knight implements Piece {
-    private Game.Type type;
+    private static Game.Type type;
+    private Box box;
+    private Player player;
 
-    public Knight(Game.Box box, Game.Player player) {
+    public Knight(Game.Box box, Game.Player player){
+        this.box = box;
+        this.player = player;
+    } 
 
+    @Override
+    public Player getPlayer(){
+        return player;
+
+    };
+    @Override
+    public Box getBox(){
+        return box;
     }
 
-    public Game.Type getType() {
-        
+    @Override
+    public void setBox(Box destinationBox){
+        this.box = destinationBox;
     }
 
+
+    public static Game.Type getType(){
+        return type;
+    }
+
+    @Override
     public boolean isValidMove(Game.Box destinationBox) {
-        
+        if (Math.abs(box.getXPosition() - destinationBox.getXPosition()) == 2 && Math.abs(box.getYPosition() - destinationBox.getYPosition()) == 1)
+			return true;
+		if (Math.abs(box.getXPosition() - destinationBox.getXPosition()) == 1 && Math.abs(box.getYPosition() - destinationBox.getYPosition()) == 2)
+			return true;
+		return false;
     }
+
 }
