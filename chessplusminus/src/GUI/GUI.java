9c42f5278ce5da3ce5a0dc7c7c44695607ff9d75
khaledsharaff <khaled.sh.02@gmail.com>
2024-05-07 23:38:18 +0300

    Added timer GUI  for players

--- chessplusminus/src/GUI/GUI.java
+++ chessplusminus/src/GUI/GUI.java
@@ -2,6 +2,8 @@ package GUI;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public interface GUI {
     public static void main(String[] args) {
@@ -15,6 +17,14 @@ public interface GUI {
         // Create and add the chess board panel
         ChessBoardGui board = new ChessBoardGui();
         frame.add(board);
+
+        // Create and add player timers
+        PlayerTimer player1Timer = new PlayerTimer(5 * 60); // 5 minutes for player 1
+        PlayerTimer player2Timer = new PlayerTimer(5 * 60); // 5 minutes for player 2
+        frame.add(player1Timer);
+        frame.add(player2Timer);
+
+        
 
         // Configure the frame
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
---
+++ chessplusminus/src/GUI/PlayerTimer.java
@@ -0,0 +1,63 @@
+package GUI;
+
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+
+import javax.swing.*;
+import javax.swing.Timer;
+
+public class PlayerTimer extends JPanel {
+    private int timeInSeconds;
+    private Timer timer;
+    private JLabel label;
+    //private Player player;
+    private boolean endTurn;
+
+    public PlayerTimer(int initialTimeInSeconds) { //Player player to be added
+        //this.player = player;
+        this.timeInSeconds = initialTimeInSeconds;
+        this.label = new JLabel();
+        this.add(label);
+        this.endTurn = false;
+
+        // Create and start the timer
+        this.timer = new Timer(1000, new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                updateTime();
+            }
+        });
+        this.timer.start();
+    }
+
+    private void updateTime() {
+        if (timeInSeconds > 0) {
+            timeInSeconds--;
+            int minutes = timeInSeconds / 60;
+            int seconds = timeInSeconds % 60;
+            label.setText(String.format("%02d:%02d", minutes, seconds));
+        } else {
+            label.setText("Time's up!");
+            timer.stop();
+            endTurn = true;
+        }
+    }
+
+    public void setEndTurn(boolean endTurn) {
+        this.endTurn = endTurn;
+        if (endTurn) {
+            timer.stop(); // Stop the timer if it's the end of the turn
+        } else {
+            timer.start(); // Start the timer if it's not the end of the turn
+        }
+    }
+
+    public boolean isEndTurn() {
+        return endTurn;
+    }
+
+    //public Player getPlayer() {
+        //return player;
+    }
+
+
