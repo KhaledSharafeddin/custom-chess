a4112a5b3925d7d80b5ac8dc29e7496ae1c499cc
emirmut1903 <emir.mut@ozu.edu.tr>
2024-05-12 21:35:01 +0300

    Game interface fixed

--- chessplusminus/src/Game/Game.java
+++ chessplusminus/src/Game/Game.java
@@ -1,7 +1,7 @@
 package Game;
 
 public interface Game {
-    public static final Player player1 = new Player();
-    public static final Player player2 = new Player();
+    public static final Player player1 = new Player(Color.WHITE, true);
+    public static final Player player2 = new Player(Color.BLACK, false);
     public void game();
 }
