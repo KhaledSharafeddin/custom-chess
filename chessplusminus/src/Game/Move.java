15413c4b4168fe2308027cd636ab17ea9544500c
Raneem <raneemabdulbar@gmail.com>
2024-05-16 02:16:05 +0300

    move added

---
+++ chessplusminus/src/Game/Move.java
@@ -0,0 +1,37 @@
+package Game;
+
+import GUI.ChessBoardGui;
+import Piece.Piece;
+
+// Move class (assuming it represents a move from a piece to a new position)
+public class Move {
+    private GUI.ChessBoardGui board;
+    private Piece piece;
+    private int newRow;
+    private int newCol;
+
+    public Move(GUI.ChessBoardGui board, Piece piece, int newRow, int newCol) {
+        this.board = board;
+        this.piece = piece;
+        this.newRow = newRow;
+        this.newCol = newCol;
+    }
+     
+    public ChessBoardGui getBoard() {
+        return board;
+    }
+
+    public Piece getPiece() {
+        return piece;
+    }
+
+    public int getNewRow() {
+        return newRow;
+    }
+
+    public int getNewCol() {
+        return newCol;
+    }
+
+    // Getter methods for board, piece, newRow, and newCol
+}
