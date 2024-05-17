9e1a846ed9ded0306d1e2b5eff38dd7052bd4727
MuratY2 <murat.yazgan@ozu.edu.tr>
2024-05-17 22:34:48 +0300

    updated piece images + king class

--- chessplusminus/src/Piece/King.java
+++ chessplusminus/src/Piece/King.java
@@ -1,23 +1,48 @@
 package Piece;
+
 //King.java
 import Game.Box;
-import Game.Color;
 import Game.Player;
+import Game.Color;
+import Game.Type;
+import GUI.ChessBoardGui;
+
+import javax.imageio.ImageIO;
 import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+import java.io.IOException;
+import java.net.URL;
 
 public class King implements Piece {
-    private static Game.Type type;
-    private static Box box;
+    private Box box;
     private Player player;
     private Color color;
-  
+    private BufferedImage image;
 
-    public King(Box box, Color color){
-        //this.player = player;
+    public King(Box box, Color color) {
         this.box = box;
         this.color = color;
+        loadImage();
     }
-    
+
+    private void loadImage() {
+        String imageName = (color == Color.WHITE) ? "king_white.png" : "king_black.png";
+        try {
+            System.out.println("Attempting to load image: pieces/" + imageName); // Debug output
+            URL resourceUrl = getClass().getResource("pieces/" + imageName);
+            if (resourceUrl == null) {
+                System.out.println("Image not found at path: pieces/" + imageName);
+            } else {
+                System.out.println("Found image at path: " + resourceUrl);
+                image = ImageIO.read(resourceUrl);
+                System.out.println("Successfully loaded image: " + imageName);
+            }
+        } catch (IOException e) {
+            System.out.println("Exception while loading image: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
+
     @Override
     public Player getPlayer() {
         return player;
@@ -34,13 +59,23 @@ public class King implements Piece {
     }
 
     public Game.Type getType() {
-        return type;
+        return Type.KING;
     }
 
-     @Override
+    @Override
     public Color getColor() {
         return color;
     }
+
+    @Override
+    public void paint(Graphics2D g2d) {
+        if (image != null) {
+            int x = box.getXPosition() * ChessBoardGui.TILE_SIZE;
+            int y = box.getYPosition() * ChessBoardGui.TILE_SIZE;
+            g2d.drawImage(image, x, y, ChessBoardGui.TILE_SIZE, ChessBoardGui.TILE_SIZE, null);
+        }
+    }
+
     @Override
     public boolean isValidMove(Game.Box destinationBox) {
         int destX = destinationBox.getXPosition();
@@ -55,8 +90,4 @@ public class King implements Piece {
         return deltaX <= 1 && deltaY <= 1;
     }
 
-    @Override
-    public void paint(Graphics2D g2d) {
-        // Implement painting logic for the King
-    }
 }
